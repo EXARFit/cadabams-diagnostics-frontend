245d483234bb107d1689d8daa847d49580bc18a1
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2025-01-13 11:45:19 +0530

    updated code

--- pages/_app.js
+++ pages/_app.js
@@ -20,19 +20,31 @@ function MyApp({ Component, pageProps }) {
           `
         }}
       />
-      
+
+      {/* Heap Analytics Script */}
+      <Script
+        id="heap-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+            heap.load("1306967079");
+          `
+        }}
+      />
+
       <AuthProvider>
         <CartProvider>
           {/* Google Tag Manager NoScript */}
           <noscript>
-            <iframe 
+            <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-TMTNG6X"
-              height="0" 
-              width="0" 
+              height="0"
+              width="0"
               style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
-          
+
           <Component {...pageProps} />
         </CartProvider>
       </AuthProvider>
