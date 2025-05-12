1dcdfc9c1dc96971e943ca4420c9aea18900e8c0
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2025-05-12 12:12:37 +0530

    navbar search fnctionality fix

--- components/home/Navbar.js
+++ components/home/Navbar.js
@@ -88,7 +88,7 @@ export default function Navbar() {
     
     setIsLoading(true);
     try {
-      const response = await fetch('https://cadabamsapi.exar.ai/api/v1/user/search/test', {
+      const response = await fetch('https://api-prod.cadabamsdiagnostics.com/api/v1/user/search/test', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
